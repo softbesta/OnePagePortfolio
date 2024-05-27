d5cadf07484fa570c187d7be27ef3ed438cc1f3e
softbesta <risingsoftdev@gmail.com>
2024-05-27 14:53:58 +0300

    portfolio

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to my portfolio
+Hi,
+I am An, a passionate full-stack software engineer with expertise in front-end and back-end technologies. I thrive on creating innovative solutions to complex problems, crafting clean and efficient code. With proficiency in multiple programming languages, frameworks, and databases, I develop scalable applications that deliver exceptional user experiences. Stay updated with the latest advancements in technology, I am constantly improving my coding skills. Coding is not just my profession, but my true passion that drives me to create elegant solutions with a positive impact.
+
+
+### Technologies Used
+* React
+* Sass
+
+## Live link
+
+[https://welcometomyportfolioo.netlify.app/](https://welcometomyportfolioo.netlify.app/)
+
+
---
+++ src/constants.js
@@ -0,0 +1,464 @@
+export const PRIMARY = 'primary'
+
+export const ResumeData = {
+  basic_info: {
+    name: 'Artur Guillou',
+    titles: ['Front-end Developer', 'Back-end Developer', 'Full-stack Developer'],
+  },
+  about: {
+    sectionTitle: 'About me',
+    description_header: 'Hi',
+    description_content: `I am a seasoned Senior Full-Stack Developer with a passion for crafting innovative web and mobile
+    applications. With a comprehensive skill set spanning frontend and backend technologies, I excel in
+    every phase of project development, from conceptualization to deployment. My track record
+    demonstrates a commitment to delivering superior solutions on time, even in high-pressure
+    environments. I am driven by a desire to leverage my expertise in software development to create
+    impactful solutions that exceed expectations.`,
+  },
+
+  projects: {
+    sectionTitle: 'Projects',
+    projects: [{
+      title: "My Portfolio",
+      startDate: "2023",
+      description: "Hello! This is my portfolio, please take a look of it. Thank You :).",
+      images: [
+        "images/portfolio/image1.png",
+        "images/portfolio/image2.png",
+        "images/portfolio/image3.png",
+        "images/portfolio/image4.png",
+        "images/portfolio/image5.png",
+      ],
+      liveUrl: "https://",
+      gitUrl: "https://",
+      technologies: [
+        {
+          class: "devicon-react-original",
+          name: "React"
+        },
+        {
+          class: "devicon-javascript-plain",
+          name: "JavaScript"
+        },
+        {
+          class: "devicon-sass-original",
+          name: "SASS"
+        },
+      ]
+    }, {
+      title: "Crypto Tracker",
+      startDate: "2022",
+      description: "Crypto Tracker is a sleek and user-friendly web app that provides real-time tracking and analysis of popular cryptocurrencies. With live price data, historical charts, Crypto Tracker is a reliable tool for crypto enthusiasts to monitor and manage their investments effectively.",
+      images: [
+        "images/crypto/image1.png",
+        "images/crypto/image2.png",
+        "images/crypto/image3.png",
+        "images/crypto/image4.png",
+        "images/crypto/image5.png"
+      ],
+      liveUrl: "https://",
+      gitUrl: "https://",
+      technologies: [
+        {
+          class: "devicon-react-original",
+          name: "React"
+        },
+        {
+          class: "devicon-javascript-plain",
+          name: "JavaScript"
+        },
+        {
+          class: "devicon-materialui-plain",
+          name: "MaterialUI"
+        }
+      ]
+    }, {
+      title: "LearningChemistry",
+      startDate: "2024",
+      description: "Welcome to LearningChemistry! (Note: Converted from a Swift/iOS app for better usage across all devices, now fully responsive) The interactive chemistry app designed especially for young learners! Whether you're curious about the elements, excited to mix up your own virtual reactions, or eager to explore the wonders of chemical compounds, LearningChemistry makes learning chemistry an engaging and enjoyable experience across all your devices. ",
+      images: [
+        "images/chemfum/image4.png",
+        "images/chemfum/image1.png",
+        "images/chemfum/image2.png",
+        "images/chemfum/image3.png",
+      ],
+      liveUrl: "https://softbesta.github.io/chemistry-web/",
+      gitUrl: "https://github.com/softbesta/chemistry-web",
+      technologies: [
+        {
+          class: "devicon-javascript-plain",
+          name: "JavaScript"
+        },
+        {
+          class: "devicon-typescript-plain",
+          name: "Typescript"
+        },
+        {
+          class: "devicon-react-original",
+          name: "React"
+        },
+        {
+          class: "devicon-sass-original",
+          name: "SCSS"
+        },
+      ]
+    }, {
+      title: "My JAM",
+      startDate: "2023",
+      description: "Introducing My JAM, a full-stack music application that offers an extensive collection of tracks, sounds, and mixes with over 3 million options for users to choose from. Developed with React for front-end and Express for back-end, My JAM also utilizes third-party APIs to provide users with a seamless and engaging listening experience. The app features robust user authentication and data protection through the implementation of JSON Web Tokens (JWT), ensuring that sensitive user data is always kept secure. Thanks to its top-notch security and vast selection of music, My JAM has increased customer satisfaction by an impressive 98%.",
+      images: [
+        "images/myJam/image1.png",
+        "images/myJam/image3.png",
+        "images/myJam/image2.png",
+        "images/myJam/image4.png",
+
+      ],
+      liveUrl: "https://",
+      gitUrl: "https://",
+      technologies: [
+        {
+          class: "devicon-react-original",
+          name: "React"
+        },
+        {
+          class: "devicon-javascript-plain",
+          name: "JavaScript"
+        },
+        {
+          class: "devicon-tailwindcss-plain",
+          name: "Tailwindcss"
+        },
+        {
+          class: "devicon-nodejs-plain",
+          name: "Node.js"
+        },
+        {
+          class: "devicon-mongodb-plain",
+          name: "MongoDB"
+        }
+      ]
+    }, {
+      title: "Vegvisits",
+      startDate: "2024",
+      description: "This is an online booking platform for that connects travelers with vegan and vegetarian locals offering accommodations.",
+      images: [
+        "images/vegtravel/image1.png",
+        "images/vegtravel/image2.png",
+        "images/vegtravel/image3.png",
+        "images/vegtravel/image4.png",
+
+      ],
+      liveUrl: "https://vegvisits.com/",
+      technologies: [
+        {
+          name: "Next",
+          class: "devicon-nextjs-original",
+          level: "100"
+        },
+        {
+          class: "devicon-typescript-plain",
+          name: "Typescript"
+        },
+        {
+          class: "devicon-javascript-plain",
+          name: "JavaScript"
+        },
+        {
+          name: "MUI",
+          class: "devicon-materialui-plain",
+          level: "100"
+        },
+        {
+          class: "devicon-nodejs-plain",
+          name: "Node.js"
+        },
+        {
+          name: "Express",
+          class: "devicon-express-original",
+          level: "100"
+        },
+        {
+          class: "devicon-mongodb-plain",
+          name: "MongoDB"
+        },
+        {
+          name: "GraphQL",
+          class: "devicon-graphql-plain",
+          level: "100"
+        },
+        {
+          name: "AWS",
+          class: "devicon-amazonwebservices-original",
+          level: "100"
+        },
+      ]
+    }, {
+      title: "What Time Is It?",
+      startDate: "2023",
+      description: "'What Time is It?' is an interactive 3D clock that automatically adjust the time based on their time zone. The visually captivating clock features a realistic three-dimensional design and is optimized for accessibility and responsiveness.",
+      images: [
+        "images/clock/image.png",
+      ],
+      liveUrl: "https://",
+      gitUrl: "https://",
+      technologies: [
+        {
+          class: "devicon-css3-plain",
+          name: "CSS"
+        },
+        {
+          class: "devicon-html5-plain",
+          name: "HTML5"
+        }
+      ]
+    }]
+  },
+
+  skills: {
+    sectionTitle: 'Skills',
+    skills: {
+      icons: [
+        {
+          name: "HTML/CSS",
+          class: "devicon-html5-plain",
+          level: "105"
+        }, {
+          name: "JavaScript",
+          class: "devicon-javascript-plain",
+          level: "100"
+        }, {
+          name: "TypeScript",
+          class: "devicon-typescript-plain",
+          level: "100"
+        }, {
+          name: "React",
+          class: "devicon-react-original",
+          level: "100"
+        }, {
+          name: "Next",
+          class: "devicon-nextjs-original",
+          level: "100"
+        }, {
+          name: "Angular",
+          class: "devicon-angularjs-plain",
+          level: "100"
+        }, {
+          name: "Vue",
+          class: "devicon-vuejs-plain",
+          level: "100"
+        }, {
+          name: "Sveltekit",
+          class: "devicon-svelte-plain",
+          level: "100"
+        }, {
+          name: "Three.js",
+          class: "devicon-threejs-original",
+          level: "100"
+        }, {
+          name: "Sass",
+          class: "devicon-sass-original",
+          level: "100"
+        }, {
+          name: "Tailwindcss",
+          class: "devicon-tailwindcss-plain",
+          level: "100"
+        }, {
+          name: "MUI",
+          class: "devicon-materialui-plain",
+          level: "100"
+        }, {
+          name: "Node",
+          class: "devicon-nodejs-plain",
+          level: "100"
+        }, {
+          name: "Express",
+          class: "devicon-express-original",
+          level: "100"
+        }, {
+          name: "Nest",
+          class: "devicon-nestjs-plain",
+          level: "100"
+        }, {
+          name: ".NET Core",
+          class: "devicon-dotnetcore-plain",
+          level: "100"
+        }, {
+          name: "Django",
+          class: "devicon-django-plain",
+          level: "100"
+        }, {
+          name: "Python",
+          class: "devicon-python-plain",
+          level: "100"
+        }, {
+          name: "Laravel",
+          class: "devicon-laravel-plain",
+          level: "100"
+        }, {
+          name: "PHP",
+          class: "devicon-php-plain",
+          level: "100"
+        }, {
+          name: "MsSQL",
+          class: "devicon-microsoftsqlserver-plain",
+          level: "100"
+        }, {
+          name: "MySQL",
+          class: "devicon-mysql-plain",
+          level: "100"
+        }, {
+          name: "MongoDB",
+          class: "devicon-mongodb-plain",
+          level: "100"
+        }, {
+          name: "PostgreSQL",
+          class: "devicon-postgresql-plain",
+          level: "100"
+        }, {
+          name: "GraphQL",
+          class: "devicon-graphql-plain",
+          level: "100"
+        }, {
+          name: "GCP",
+          class: "devicon-googlecloud-plain",
+          level: "100"
+        }, {
+          name: "Docker",
+          class: "devicon-docker-plain",
+          level: "100"
+        }, {
+          name: "Jest",
+          class: "devicon-jest-plain",
+          level: "100"
+        }, {
+          name: "AWS",
+          class: "devicon-amazonwebservices-original",
+          level: "100"
+        }, {
+          name: "Azure",
+          class: "devicon-azure-plain",
+          level: "100"
+        }
+      ]
+    }
+  },
+
+  experiences: {
+    sectionTitle: 'Experiences',
+    experiences: [
+      {
+        company: "-VEGVISITS-",
+        title: "Senior Fullstack Engineer",
+        years: "Aug.2022 - May.2024",
+        mainTech: [
+          "Fullstack Development"
+        ],
+        technologies: [
+          "TypeScript",
+          "Next.js",
+          "ReduxThunk",
+          "Material UI",
+          "Angular",
+          "Node.js",
+          "Express.js",
+          "GraphQL",
+          "MongoDB",
+          "AWS",
+        ]
+      },
+      {
+        company: "-AMIGOAI-",
+        title: "Senior Fullstack Engineer",
+        years: "Nov.2020 - Jun.2022",
+        mainTech: [
+          "Fullstack Development (Android/IOS/Web)"
+        ],
+        technologies: [
+          "TypeScript",
+          "React.js",
+          "React Native",
+          "Three.js",
+          "Nest.js",
+          "REST API",
+          "DynamoDB",
+          "Docker",
+          "AWS",
+          "Jest",
+        ]
+      },
+      {
+        company: "-EAZIFUNDS-",
+        title: "Fullstack Engineer",
+        years: "July.2018 - Sep.2020",
+        mainTech: [
+          "Fullstack Development (Android/Web)"
+        ],
+        technologies: [
+          "Typescript",
+          "Next.js",
+          "Redux Toolkit",
+          "React.js",
+          "Ionic Framework",
+          "PHP",
+          "Node.js",
+          "Express.js",
+          "Laravel",
+          "Django",
+        ]
+      },
+      {
+        company: "-INTEGRIFY-",
+        title: "Full Stack Development Fellow",
+        years: "Oct.2017 - Jun.2018",
+        mainTech: [
+          "Fullstack Development"
+        ],
+        technologies: [
+          "React.js",
+          "Next.js",
+          "Angular",
+          "ASP.NET Core",
+          "MSSQL",
+          "Azure",
+          "MongoDB",
+          "Node.js",
+          "Nest.js",
+          "Laravel",
+        ]
+      },
+      {
+        company: "-TOMISHA-",
+        title: "Web Developer",
+        years: "Jun.2014 - Sep.2017",
+        mainTech: [
+          "Web Development"
+        ],
+        technologies: [
+          "Vue.js",
+          "Nuxt.js",
+          "Vuex",
+          "React.js",
+          "Node.js",
+          "Express.js",
+          "MongoDB",
+        ]
+      },
+    ]
+  },
+
+  socials: {
+    auth: 'Artur Guillou',
+    networks: [
+      {
+        name: "github",
+        url: "https://github.com/softbesta",
+        icon: "fab fa-github"
+      },
+      {
+        name: "linkedin",
+        url: "https://www.linkedin.com/in/artursoft1/",
+        icon: "fab fa-linkedin"
+      }
+    ],
+  }
+}
